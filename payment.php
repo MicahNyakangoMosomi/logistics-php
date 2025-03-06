7a18a9a8a9560e5a22d1bbea7e212c79a2bf5796
Cipher <nyakangomicah4@gmail.com>
2025-03-06 12:20:31 +0300

    payment.html

---
+++ payment.php
@@ -0,0 +1,64 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="assets/css/main.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="assets/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
+    <link href="assets/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="assets/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+    <link href="assets/vendor/aos/aos.css" rel="stylesheet">
+    <title>Payment Page</title>
+    <style>
+        .mpesa-logo{
+            height: auto;
+            width: 200px;
+        }
+    </style>
+    <script>
+        function getQueryParam(param) {
+            var urlParams = new URLSearchParams(window.location.search);
+            return urlParams.get(param);
+        }
+
+
+        window.onload = function() {
+            var amount = getQueryParam('amount');
+            document.getElementById('amount').value = amount || '';
+        }
+    </script>
+</head>
+<body class="d-flex justify-content-center align-items-center" style="height: 100vh; background-color: rgb(182, 188, 194);">
+
+    <main id="login" class="container border-2 border-secondary p-5">
+        <div class="row">
+            <div class="col-lg-4 bg-light d-flex align-items-center text-white rounded-start">
+                <div class="text-center w-100 p-3">
+                    <img  src="./assets/img/mpesa logo.png" class="mpesa-logo" alt="">
+                    <h3 class="fw-bold text-success">WELCOME</h3>
+                    <p class="fw-light text-success">We are thrilled to have you here. Thank you for considering us and for placing your trust in Muhidin Logistics. Your satisfaction and trust are paramount to us.</p>
+                </div>
+            </div>
+
+            <div class="col-lg-8 bg-white rounded-end">
+                <div class="wrapper my-5">
+                    <form id="paymentForm" action="./stk_initiate.php" method="POST" class="user-input-2 p-5">
+                        <div class="form-group mb-4">
+                            <input type="text" class="form-control" name="phone" placeholder="M-Pesa Number" maxlength="12" title="Please enter Mpesa number" required>
+                        </div>
+                        <div class="form-group mb-4">
+                            <input type="text" id="amount" class="form-control" name="amount"placeholder="Amount" readonly required>
+                        </div>
+                        <div class="container mt-3">
+                            <button type="submit" name="submit" value="submit" class="btn btn-success w-100" id="pay">Proceed</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </main>
+    <script src="./assets/js/main.js"></script>  
+</body>
+</html>
